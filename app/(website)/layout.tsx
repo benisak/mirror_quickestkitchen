484c06e0be3a1b598a7b23347da1a1f73dab6d32
CrisJurado13 <cristianj@dreamcode.io>
2025-07-15 12:43:15 -0500

    GTM updated

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -35,8 +35,8 @@ async function sharedMetaData(params: any) {
     title: {
       default:
         settings?.title ||
-        "Quickest Kitchen | Quick & Delicious Recipes for Busy Lives",
-      template: "%s | Quickest Kitchen | Quick & Delicious Recipes for Busy Lives"
+        "Quickest-recipes | Quick & Delicious Recipes for Busy Lives",
+      template: "%s | Quickest-recipes | Quick & Delicious Recipes for Busy Lives"
     },
     description:
       settings?.description ||
@@ -67,7 +67,7 @@ async function sharedMetaData(params: any) {
 }
 
 
-const siteUrl = "https://www.quickestkitchen.com/"; // Ensure this is correct
+const siteUrl = "https://www.quickest-recipes.com/"; // Ensure this is correct
 
 export async function generateMetadata({ params }: { params: any }) {
   const settings = await getSettings();
@@ -159,10 +159,10 @@ export default async function Layout({
         <script
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-PXGJ36HN');`
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5R934B83');`
           }}
         />
         {/* End Google Tag Manager */}
@@ -175,7 +175,7 @@ export default async function Layout({
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PXGJ36HN"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5R934B83"
             height="0" 
             width="0" 
             style={{ display: 'none', visibility: 'hidden' }}
